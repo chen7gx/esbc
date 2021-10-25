933385338337092f5e7816ea905d200a3d2982f3
chen7gx <gxissun@gmail.com>
2021-10-26 01:23:10 +0800

    unlock

---
+++ server/flags.go
@@ -0,0 +1,30 @@
+package server
+
+// Tendermint full-node start flags
+const (
+	flagWithTendermint = "with-tendermint"
+	flagAddress        = "address"
+	flagTransport      = "transport"
+	flagTraceStore     = "trace-store"
+	flagCPUProfile     = "cpu-profile"
+)
+
+// GRPC-related flags.
+const (
+	flagGRPCEnable  = "grpc.enable"
+	flagGRPCAddress = "grpc.address"
+)
+
+// RPCAPI-related flags.
+const (
+	flagRPCAPI    = "rpc-api"
+	flagUnlockKey = "unlock-key"
+)
+
+// Ethereum-related flags.
+const (
+	flagJSONRPCEnable            = "json-rpc.enable"
+	flagJSONRPCAddress           = "json-rpc.address"
+	flagEthereumWebsocketEnable  = "ethereum-websocket.enable"
+	flagEthereumWebsocketAddress = "ethereum-websocket.address"
+)
